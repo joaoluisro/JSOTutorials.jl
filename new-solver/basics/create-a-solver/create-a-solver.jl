3f193f2a9974062f47e4127331426cc29689fd4a
Travis CI User <travis@example.org>
2020-01-12 19:31:29 +0000

    :robot: Building tutorials pages

--- new-solver/basics/create-a-solver/create-a-solver.jl
+++ new-solver/basics/create-a-solver/create-a-solver.jl
@@ -6,6 +6,7 @@ if false
  pkg"add SolverTools"
  pkg"add SolverBenchmark"
  pkg"add Plots"
+ pkg"add PyPlot"
  pkg"add LinearAlgebra"
  pkg"add JSOSolvers"
 end
@@ -21,35 +22,47 @@ function newton(nlp :: AbstractNLPModel;
                 max_time :: Float64 = 30.0,
                 max_iter :: Int = 100)
 
+	fx = obj(nlp, x)
+	∇fx = grad(nlp, x)
+	nrmgrad = norm(∇fx)
+	∇²fx = Symmetric(hess(nlp, x), :L)
+
   T = eltype(x)
   k = 0
   el_time = 0.0
   start_time = time()
   tired = el_time > max_time || k ≥ max_iter
-  optimal = norm(grad(nlp, x)) < max_tol
+  optimal = nrmgrad < max_tol
 
   @info log_header([:iter, :f, :nrmgrad], [Int, T, T], hdr_override=Dict(:f => "f(x)", :nrmgrad => "‖∇f(x)‖"))
 
   while !(optimal || tired)
 
-    fx = obj(nlp, x)
-    ∇fx = grad(nlp, x)
-    nrmgrad = norm(∇fx)
-    ∇²fx = Symmetric(hess(nlp, x), :L)
-
     @info log_row(Any[k, fx, nrmgrad])
 
-    d = isposdef(∇²fx) ? ∇²fx\ -∇fx : -∇fx
+    newton_step = -∇²fx\∇fx
+    d = dot(step, -∇fx) < 0 ? newton_step : -∇fx
     t = one(T)
     α = 0.5
+		slope = dot(∇fx, t*d)
+		xt = x + t*d
+		ft = obj(nlp, xt)
 
-    while obj(nlp, x + t*d) > fx + α * ∇fx' * t * d
+    while ft > fx + α * slope
       t *= 0.5
+			xt = x + t*d
+			ft = obj(nlp, xt)
+			slope =  dot(∇fx, t*d)
     end
 
     x += t*d
 
-    k += 1
+		fx = obj(nlp, x)
+    ∇fx = grad(nlp, x)
+    nrmgrad = norm(∇fx)
+    ∇²fx = Symmetric(hess(nlp, x), :L)
+
+		k += 1
     el_time = time() - start_time
     tired = el_time > max_time || k ≥ max_iter
     optimal = nrmgrad < max_tol
@@ -67,48 +80,49 @@ function newton(nlp :: AbstractNLPModel;
     status =:unknown
   end
 
-  return GenericExecutionStats(status, nlp, solution=x, objective=obj(nlp, x),
+  return GenericExecutionStats(status, nlp, solution=x, objective=fx,
                                iter = k, elapsed_time = el_time)
 
 end
 
 
-  f(x) = (x[1]^2 + x[2]^2)^2
-  problem = ADNLPModel(f, [1.0,2.0])
-  output = newton(problem)
+	using SolverTools
+	SolverTools.show_statuses()
 
 
-  print(output)
+f(x) = (x[1]^2 + x[2]^2)^2
+problem = ADNLPModel(f, [1.0,2.0])
+output = newton(problem)
+
+
+print(output)
 
 
 using SolverBenchmark, JSOSolvers
 solvers = Dict(:newton=>newton, :lbfgs=>lbfgs)
 f1(x) = x[1]^2 + x[2]^2
 f2(x) = (1 - x[1])^2 + 100(x[2] - x[1]^2)^2
-f3(x) = (x[1]^2 + x[2] - 11) + (x[1] + x[2]^2 - 7)^2
-f4(x) = 0.26(x[1]^2 + x[2]^2) - 0.48*x[1]*x[2]
+f3(x) = x[1]^2 + x[2] - 11 + (x[1] + x[2]^2 - 7)^2
+f4(x) = 0.26 * (x[1]^2 + x[2]^2) - 0.48 * x[1] * x[2]
 test_functions = [f1, f2, f3, f4]
-problems = (ADNLPModel(i, 2*ones(2),name="Problem $i") for i in test_functions)
+problems = (ADNLPModel(i, 2*ones(2), name="Problem $i") for i in test_functions)
 stats = bmark_solvers(solvers, problems)
 
 
-open("newton.tex","w") do io
-	latex_table(io,stats[:newton],cols = [:name,:status,:objective,:elapsed_time,:iter])
+open("newton.tex", "w") do io
+  latex_table(io, stats[:newton], cols = [:name, :status, :objective, :elapsed_time, :iter])
 end
-open("lbfgs.tex","w") do io
-	latex_table(io,stats[:lbfgs],cols = [:name,:status,:objective,:elapsed_time,:iter])
+open("lbfgs.tex", "w") do io
+  latex_table(io, stats[:lbfgs], cols = [:name, :status, :objective, :elapsed_time, :iter])
 end
 
 
-open("newton.md","w") do io
-	markdown_table(io,stats[:newton],cols = [:name,:status,:objective,:elapsed_time,:iter])
-end
-open("lbfgs.md","w") do io
-	markdown_table(io,stats[:lbfgs],cols = [:name,:status,:objective,:elapsed_time,:iter])
-end
+markdown_table(stdout, stats[:newton],cols = [:name,:status,:objective,:elapsed_time,:iter])
+
+markdown_table(stdout, stats[:lbfgs],cols = [:name,:status,:objective,:elapsed_time,:iter])
 
 
 using Plots
-plotly()
+pyplot()
 performance_profile(stats, df->df.elapsed_time)
 
